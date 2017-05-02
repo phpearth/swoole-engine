758f98f794f84ade7625e79282744e38052a985c
Peter Kokot <peterkokot@gmail.com>
2017-05-03 01:16:57 +0200

    Simplify creation of  superglobal for Symfony driver

--- src/Driver/Symfony/Request.php
+++ src/Driver/Symfony/Request.php
@@ -7,30 +7,16 @@ use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 class Request
 {
     /**
-     * Creates Symfony request from Swoole request. By default Swoole request
-     * contains headers with lower case keys and dash separator instead
-     * of underscores and upper case as PHP will expect later on in $_SERVER
-     * superglobal. For example:
-     * - host: localhost:9501
-     * - connection: keep-alive
-     * - accept-language: en-US,en;q=0.8,sl;q=0.6
-     * Also PHP superglobals must get set here.
+     * Creates Symfony request from Swoole request. PHP superglobals must get set
+     * here.
      *
      * @param  \swoole_http_request $request
      * @return Request
      */
-    public function createSymfonyRequest(\swoole_http_request $request) {
-        // $_SERVER
-        $headers = [];
-        foreach ($request->header as $key => $value) {
-            $key = str_replace('-', '_', $key);
-            $key = ucwords($key, '-');
-            $headers['http_' . $key] = $value;
-        }
-        $_SERVER = array_merge($request->server, $headers);
-        // Also change possible remaining keys to uppercase
-        $_SERVER = array_change_key_case($_SERVER, CASE_UPPER);
-        $request->server = $_SERVER;
+    public function createSymfonyRequest(\swoole_http_request $request)
+    {
+        // Create $_SERVER for traditional PHP applications
+        $_SERVER = array_change_key_case($request->server, CASE_UPPER);
 
         // Other superglobals
         $_GET = $request->get ?? [];
