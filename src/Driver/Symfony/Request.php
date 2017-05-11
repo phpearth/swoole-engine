9e937786aa5b9c843495f040c0e8bf970ff6ff50
Peter Kokot <peterkokot@gmail.com>
2017-05-11 15:32:06 +0200

    Add creation of  superglobal

--- src/Driver/Symfony/Request.php
+++ src/Driver/Symfony/Request.php
@@ -15,8 +15,7 @@ class Request
      */
     public function createSymfonyRequest(\swoole_http_request $request)
     {
-        // Create $_SERVER for traditional PHP applications
-        $_SERVER = array_change_key_case($request->server, CASE_UPPER);
+        $this->setServer($request);
 
         // Other superglobals
         $_GET = $request->get ?? [];
@@ -41,5 +40,44 @@ class Request
         }
 
         return $symfonyRequest;
+    }
+
+    /**
+     * Create $_SERVER superglobal for traditional PHP applications. By default
+     * Swoole request contains headers with lower case keys and dash separator
+     * instead of underscores and upper case letters which PHP expects in the
+     * $_SERVER superglobal. For example:
+     * - host: localhost:9501
+     * - connection: keep-alive
+     * - accept-language: en-US,en;q=0.8,sl;q=0.6
+     *
+     * @param \swoole_http_request $request
+     */
+    public function setServer($request)
+    {
+        $headers = [];
+
+        // For just in case security we'll whitelist the expected headers. No more,
+        // should be added to $_SERVER.
+        $whitelist = [
+            'HTTP_HOST',
+            'HTTP_CONNECTION',
+            'HTTP_USER_AGENT',
+            'HTTP_ACCEPT',
+            'HTTP_ACCEPT_ENCODING',
+            'HTTP_ACCEPT_LANGUAGE',
+            'HTTP_REFERER',
+            'HTTP_ACCEPT_CHARSET'
+        ];
+
+        foreach ($request->header as $key => $value) {
+            $headerKey = 'HTTP_' . strtoupper(str_replace('-', '_', $key));
+            if (in_array($headerKey, $whitelist)) {
+                $headers[$headerKey] = $value;
+            }
+        }
+
+        // Make swoole's server's keys uppercased and merge them into the $_SERVER superglobal
+        $_SERVER = array_change_key_case(array_merge($request->server, $headers), CASE_UPPER);
     }
 }
